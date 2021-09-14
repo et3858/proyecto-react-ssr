8f2085e2c2c615bd1e3d9cb6d4537c87d0bb475f
Eduardo Tejada <et3858@gmail.com>
2021-09-14 16:39:23 -0300

    Instalando clean-webpack-plugin y aplicándolo en webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 const webpackConfig = {
     entry: "./src/index.js",
@@ -52,6 +53,7 @@ const webpackConfig = {
         historyApiFallback: true
     },
     plugins: [
+        new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             template: "./public/index.html",
             filename: "./index.html"
